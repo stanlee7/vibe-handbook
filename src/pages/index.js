2a52fc3f7e2d3f8c441acbadde8031bec7ea41b1
stanlee <stanlee3927@gmail.com>
2026-06-17 10:47:22 +0900

    Fix homepage CTA link and Korean copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/">
+            핸드북 시작하기 →
           </Link>
         </div>
       </div>
